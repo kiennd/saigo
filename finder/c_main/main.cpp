ea88397b851a7d46650c9fb54fb344c8c2027c9f
ljchrono <ljchrono@9e696486-f14e-0410-aed7-491311e747fa>
2008-10-08 21:59:34 +0000

    Dividido a funcao de salvar regioes em varias funcoes, numa forma mais modular

--- finder/c_main/main.cpp
+++ finder/c_main/main.cpp
@@ -12,99 +12,116 @@
 
 using namespace std;
 
+IplImage* getRectImage (IplImage *input,Rect& r,int b) {
+	IplImage *region;
 
-void findSpaces (IplImage* input) {
+	CvRect retInteresse = cvRect(r.left-b,r.top-b,(r.right-r.left)+b,(r.bottom-r.top)+b);
+	region = cvCreateImage(cvSize(retInteresse.width,retInteresse.height),input->depth,input->nChannels);
+	cvSetImageROI( input, retInteresse );
+    cvCopy( input, region, NULL );
+	cvResetImageROI(input);
 
-	int meioAltura = input->height / 2;
-  int x;
-  bool ehEspaco;
-  int inicio = 0 , fim =0; 
-  CvScalar pixel, pixel2;
-  for (x=0;x<input->width;x++) {
-    ehEspaco = false;
-    pixel = cvGet2D(input,meioAltura,x);
-    if (pixel.val[0] == 0) {
-      int y;
-      ehEspaco = true;
-      for (y=0;y<input->height;y++) {
-        pixel = cvGet2D(input,y,x);
-        if (pixel.val[0] != 0)
-          ehEspaco = false;      
-      }
-      if (ehEspaco) {
-      	if ( inicio == 0 )
-      		inicio = x;
-      	else
-      		fim = x;
-      	
-      } else {
-      	if ( inicio != 0 ) {
-	      	cout << "pixel de espaço: " << inicio << "," << fim << "\n";
-  	    	inicio = fim = 0;      
-  	    }
-      }  
-    }
-  }
-
+	return region;
 }
 
-void salvaRegioes (IplImage* input, vector<Rect> *rects, char* outputFileName, CvScalar mediaCorInteresse, int b) {
-	IplImage* imgTeste;
-	CvRect retInteresse;
-	char caminhoArquivo[255];
-	int i = 1;
-	CvScalar s;
-	vector<Rect>::iterator it;
-	char diretorio[255];
-	char comando[255];
-	sprintf(diretorio,"%s_dir",outputFileName);
-	sprintf(comando,"mkdir %s",diretorio);	
-	system(comando);	
+int getThreshold (CvScalar mediaCorInteresse) {
+
 	int limiarBinarizacao;	
 	limiarBinarizacao = (int) mediaCorInteresse.val[0];
 	if (mediaCorInteresse.val[1] > limiarBinarizacao)
 	  limiarBinarizacao = (int) mediaCorInteresse.val[1];
 	if (mediaCorInteresse.val[2] > limiarBinarizacao)
 	  limiarBinarizacao = (int) mediaCorInteresse.val[2];	
-  cout << "Limiar binarização calculado: " << limiarBinarizacao << "\n";
-	  
-	for ( it = rects->begin() ; it != rects->end() ; it++ ) {
-	  if ((it->right - it->left > 0) && (it->bottom-it->top > 0)) {
-	    retInteresse = cvRect(it->left-b,it->top-b,(it->right-it->left)+b,(it->bottom-it->top)+b);
-	    imgTeste = cvCreateImage(cvSize(retInteresse.width,retInteresse.height),input->depth,input->nChannels);
-	    cvSetImageROI( input, retInteresse );
-            cvCopy( input, imgTeste, NULL );
-            
-	    int x,y;
-	    //limiarBinarizacao = (mediaCorInteresse.val[0] + mediaCorInteresse.val[1] + mediaCorInteresse.val[2])/3;
-	    
-	    for (x=0;x<imgTeste->height;x++)
-	      for (y=0;y<imgTeste->width;y++) {
-	        s = cvGet2D(imgTeste,x,y);
+
+	return limiarBinarizacao;
+}
+
+IplImage* binarize (IplImage *input,int limiarBinarizacao) {
+	int x,y;
+	CvScalar s;
+	for (x=0;x<input->height;x++) for (y=0;y<input->width;y++) {
+		s = cvGet2D(input,x,y);
 		if ((s.val[0] > limiarBinarizacao) || (s.val[1] > limiarBinarizacao) || (s.val[2] > limiarBinarizacao)) {
-		  s.val[0] = 255;
-		  s.val[1] = 255;
-		  s.val[2] = 255;
+			s.val[0] = s.val[1] = s.val[2] = 255;
+		} else {
+			s.val[0] = s.val[1] = s.val[2] = 0;
 		}
-		else {
-		  s.val[0] = 0;
-		  s.val[1] = 0;
-		  s.val[2] = 0;
-		}	  
+		cvSet2D(input,x,y,s);
+	}
+	return input;
+}
 
-		cvSet2D(imgTeste,x,y,s);
-	        	
-		
-	      }
+bool isSpaceColumn (IplImage *input,int col) {
+	bool ehEspaco = true;
+	CvScalar pixel;
+	int y;
+	for (y=0;y<input->height;y++) {
+      pixel = cvGet2D(input,y,col);
+      if (pixel.val[0] != 0)
+        ehEspaco = false;      
+    }
+	return ehEspaco;
+}
 
-			sprintf(caminhoArquivo,"%s/region%d.bmp",diretorio,i);
-            findSpaces(imgTeste);
-            cvSaveImage(caminhoArquivo,imgTeste);
-            i++;
-            cvResetImageROI(input);
-          }
-    //cout << "dimensoes ret: " << it->right - it->left << " " << it->bottom-it->top << "\n" ;
-        }
+void findSpaces (IplImage* input) {
+
+	int meioAltura = input->height / 2;
+	int x;
+	int inicio = 0 , fim =0; 
+	CvScalar pixel;
+
+	for (x=0;x<input->width;x++) {
+	    pixel = cvGet2D(input,meioAltura,x);
+		if (pixel.val[0] == 0) {
+			if ( isSpaceColumn (input,x) )
+				if ( inicio == 0 )
+      				inicio = x;
+			    else
+      				fim = x;
+      	
+		} else {
+			if ( fim == 0 )
+				fim = inicio;
+
+			if ( inicio != 0 ) {
+				cout << "pixel de espaço: " << inicio << "," << fim << "\n";
+	  	    	inicio = fim = 0;      
+  		    }
+   		}	   
+   
+  	}
+}
+
+void salvaRegioes (IplImage* input, vector<Rect> *rects, char* outputFileName, CvScalar mediaCorInteresse, int b) {
+	IplImage* imgTeste;
+	char caminhoArquivo[255];
+	char diretorio[255];
+	char comando[255];
+	int i = 1;
+
+	sprintf(diretorio,"%s_dir",outputFileName);
+	sprintf(comando,"bash -c '[[ -d %s ]] || mkdir %s'",diretorio,diretorio);	
+	system(comando);
+
+	int limiarBinarizacao = getThreshold(mediaCorInteresse);	
+  	cout << "Limiar binarização calculado: " << limiarBinarizacao << "\n";
+	 
+	int j;
+	Rect r;
+	for ( j = 0 ; j < rects->size() ; j++ ) {
+	  r = rects->at(j);  
+	
+	  if ((r.right - r.left > 0) && (r.bottom-r.top > 0)) {
+	              
+		imgTeste = getRectImage (input,r,b);
+		imgTeste = binarize(imgTeste,limiarBinarizacao);
+            
+		sprintf(caminhoArquivo,"%s/region%d.bmp",diretorio,i);
+        findSpaces(imgTeste);
+        cvSaveImage(caminhoArquivo,imgTeste);
+        i++;
+      }
+	}
 }
 
 void execute (char *inputF,char *outputF,int v,int l,int s,int rs, bool salvarRetangulos, int b) {
@@ -113,18 +130,18 @@ void execute (char *inputF,char *outputF,int v,int l,int s,int rs, bool salvarRe
 	if (!input) {
 		print::error ("Erro:","Arquivo de entrada não existe.");
 	}	
-        CvScalar mediaCorInteresse;
+	CvScalar mediaCorInteresse;
 	vector<Range> *r = findvar::get_ranges(input,v,s,rs,&mediaCorInteresse);
 	vector<Rect> *rects = bbox::get(input,r,l);
 
 	cout << "Encontrou " << rects->size() << " Regioes de Texto." <<  endl;
-  if (salvarRetangulos) {
-   	salvaRegioes(input, rects, outputF, mediaCorInteresse, b);
-  }
+  	if (salvarRetangulos) 
+   		salvaRegioes(input, rects, outputF, mediaCorInteresse, b);
+	
 	vector<Rect>::iterator it;
 	for ( it = rects->begin() ; it != rects->end() ; it++ ) {
-    cvRectangle(input, cvPoint(it->left,it->top), cvPoint(it->right,it->bottom), cvScalar(0,0,255), 1);
-  }
+		cvRectangle(input, cvPoint(it->left,it->top), cvPoint(it->right,it->bottom), cvScalar(0,0,255), 1);
+	}
 
 	if(!cvSaveImage(outputF,input)) 
 		print::error("Erro:","Não foi possivel salvar em "+string(outputF)+ ".");
@@ -134,13 +151,13 @@ void execute (char *inputF,char *outputF,int v,int l,int s,int rs, bool salvarRe
 void display (vector<IplImage*> *images) {
 
 	int i = 0;
-	cvNamedWindow("nham", CV_WINDOW_AUTOSIZE); 
-    cvMoveWindow("nham", 100, 100);
+	cvNamedWindow("window", CV_WINDOW_AUTOSIZE); 
+    cvMoveWindow("window", 100, 100);
 
 	if (images->size() <= 0 )
 		return;
 	else {
-		cvShowImage("nham",images->at(0));
+		cvShowImage("window",images->at(0));
 	}
 
 	char key;
@@ -157,14 +174,14 @@ void display (vector<IplImage*> *images) {
 		} else {
 			break;
 		}
-		cvShowImage("nham",images->at(i));
+		cvShowImage("window",images->at(i));
 	} while(1);
 }
 
 int main ( int argc , char **argv )
 {	
-	char *inputF = NULL, *outputF = "output_image.ppm";
-	int v = 12,l = 5,s = 60,r = 10,b=0;
+	char *inputF = NULL, *outputF = (char *) "output_image.ppm";
+	int v = 15,l = 6,s = 100,r = 8,b=0;
 	bool salvarRetangulos = false;
 	bool mostrarResultado = false;
 	do {
@@ -182,7 +199,7 @@ int main ( int argc , char **argv )
 			case 'l': l = atoi(optarg); break;
 			case 's': s = atoi(optarg); break;
 			case 'r': r = atoi(optarg); break;
-  		case 'b': b = atoi(optarg); break;
+			case 'b': b = atoi(optarg); break;
 			case 'g': salvarRetangulos = true; break;
 			case 'w': mostrarResultado = true; break;
 			case 'h':
